709e60395061016df7633c70f4d1efd77b89cf90
yms2 <dbalstjd789@daum.net>
2025-07-30 17:54:14 +0900

    ESLint 설정 파일을 주석 처리하고, 새로운 ESLint 구성으로 변경하여 모든 파일을 무시하도록 설정

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,37 +1,37 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['tsconfig.json'],
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint', 'prettier', 'import'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
-  rules: {
-    'prettier/prettier': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unsafe-assignment': 'warn',
-    '@typescript-eslint/no-unsafe-member-access': 'warn',
-    '@typescript-eslint/no-unsafe-call': 'warn',
-    '@typescript-eslint/no-unsafe-return': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-};
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: ['tsconfig.json'],
+//     tsconfigRootDir: __dirname,
+//     sourceType: 'module',
+//   },
+//   plugins: ['@typescript-eslint', 'prettier', 'import'],
+//   extends: [
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+//     'plugin:prettier/recommended',
+//     'plugin:import/errors',
+//     'plugin:import/warnings',
+//     'plugin:import/typescript',
+//   ],
+//   root: true,
+//   env: {
+//     node: true,
+//     jest: true,
+//   },
+//   ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
+//   rules: {
+//     'prettier/prettier': 'warn',
+//     '@typescript-eslint/no-explicit-any': 'warn',
+//     '@typescript-eslint/no-unsafe-assignment': 'warn',
+//     '@typescript-eslint/no-unsafe-member-access': 'warn',
+//     '@typescript-eslint/no-unsafe-call': 'warn',
+//     '@typescript-eslint/no-unsafe-return': 'warn',
+//     '@typescript-eslint/explicit-function-return-type': 'warn',
+//   },
+//   settings: {
+//     'import/resolver': {
+//       typescript: {},
+//     },
+//   },
+// };
